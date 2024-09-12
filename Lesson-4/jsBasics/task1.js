b94bbeb6976fba12dce2bd600960c9850dd55b35
“Valeriia <bezditna.valeriya@gmail.com>
2024-09-12 12:52:54 +0200

    Lesson 4. HomeWork3

---
+++ Lesson-4/jsBasics/task1.js
@@ -0,0 +1,25 @@
+let firstNameLastName = 'Prosto Valeriia';
+let page = 7;
+let hasPassport = true;
+let age = null;
+let phone = undefined;
+
+console.log(firstNameLastName);
+console.log(page);
+console.log(hasPassport);
+console.log(age);
+console.log(phone);
+//----- const
+
+const bookName = "You Don\'t Know JS Yet: Get Started";
+const pages = 187;
+const isAuthor = true;
+const publicationDate = null;
+const stock = undefined;
+
+console.log(bookName);
+console.log(pages);
+console.log(isAuthor);
+console.log(publicationDate);
+console.log(stock);
+
---
+++ Lesson-4/jsBasics/task2.js
@@ -0,0 +1,6 @@
+const firstPerson= "Arthur Conan Doyle";
+const secondPerson = "Pavlo Zagrebeljniy";
+const greeting = "Hello, Mr." + firstPerson + " and Mr." + secondPerson;
+console.log(greeting);
+const greeting2 = `Hello, Mr. ${firstPerson} and Mr. ${secondPerson}`;
+console.log(greeting2);

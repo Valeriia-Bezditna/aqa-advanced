886489e66497fc92368971abb57304705e2ea737
“Valeriia <bezditna.valeriya@gmail.com>
2024-09-14 11:42:58 +0200

    Lesson5. HomeWork4.

---
+++ Lesson-5/jsBasics/task3.js
@@ -0,0 +1,20 @@
+//------for
+console.log("Multiplication by 7");
+const number = 7;
+
+for (let i = 1; i <= 9; i++) {
+
+    console.log(number + " " + "X" + " " + i + " " + '=' + " " + number * i);
+
+}
+
+//----While
+
+console.log("Multiplication by 5");
+const number1 = 5;
+let a = 1;
+while (a <= 9) {
+    console.log(number1 + " " + "X" + " " + a + " " + '=' + " " + number1 * a);
+    a++;
+}
+

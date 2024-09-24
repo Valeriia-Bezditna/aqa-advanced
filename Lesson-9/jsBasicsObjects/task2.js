31a056876d1e8d117fe43619035c005c4fa2e738
“Valeriia <bezditna.valeriya@gmail.com>
2024-09-24 22:06:57 +0200

    Lesson9. HomeWork8

---
+++ Lesson-9/jsBasicsObjects/task2.js
@@ -0,0 +1,9 @@
+const book = {
+    title: "Charlie and the Chocolate Factory",
+    author: "Roald Dahl",
+    year: 2010
+
+};
+const { title, author } = book;
+console.log(`Book: ${title}`);
+console.log(`author: ${author}`);

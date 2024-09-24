7a0b497ece8810fc08f3af70ec7885cbfa92c8cb
“Valeriia <bezditna.valeriya@gmail.com>
2024-09-24 14:52:20 +0200

    Lesson8. HomeWork7

---
+++ Lesson-8/jsBasicsArray/task2.js
@@ -0,0 +1,3 @@
+const array = [1, 2, 3, 4, 5];
+const doubledArray = array.map((value, index) => value * index);
+console.log(doubledArray);
---
+++ Lesson-8/jsBasicsArray/task4.js
@@ -0,0 +1,3 @@
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenNum = num.filter((value) => value % 2 === 0);
+console.log(evenNum);
